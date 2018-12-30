8df71bafeb4a664c4c1be2d9291bc0454abb1b9c
JuloWaks <JuloWaks@users.noreply.github.com>
2018-12-30 03:10:26 +0200

    adds java.nio functions

--- src/SockHandler.java
+++ src/SockHandler.java
@@ -3,35 +3,40 @@ import jdk.internal.util.xml.impl.Input;
 
 import java.net.*;
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedSelectorException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class SockHandler extends Thread {
-    private Socket clientSocket = null;
+    private SocketChannel clientSocket = null;
     private int HEADER_SIZE = 8;
     private byte[] head;
-    private Socket serverSocket ;
-    public SockHandler(Socket socket) {
+    private Map<String,Object> headerValues;
+    private SocketChannel serverSocket = null ;
+    public SockHandler(SocketChannel socket) {
         super("SockHandler");
         this.clientSocket = socket; // TOOD set timeout
     }
 
-    private Map<String,Object> headerParsing(InputStream in) throws UnknownHostException {
+    private Map<String,Object> headerParsing(SocketChannel sock) throws UnknownHostException {
         Map<String,Object> headerValues = new HashMap<String, Object>();
 
         int response = 0;
-        byte[] headers = new byte[500];
+        byte[] headers;
+        
+        ByteBuffer buf = ByteBuffer.allocate(500);
+        buf.clear();
         try {
-           response = in.read(headers);
+           response = sock.read(buf);
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        if(response != HEADER_SIZE){
-//            throw new Error("Could not read the whole header datagram");
-//        }
+        buf.flip();
+        headers = buf.array();
         headerValues.put("VERSION", (int) headers[0]);
         headerValues.put("COMMAND", headers[1]);
         headerValues.put("PORT", (headers[3] & 0xFF) | ((headers[2] & 0xFF)<< 8));
@@ -41,23 +46,29 @@ public class SockHandler extends Thread {
 
         return headerValues;
     }
-    private void sendResponse(OutputStream out) throws IOException {
-        byte[] response = new byte[]{0, 90, 0, 80, head[4], head[5], head[6], head[7]};
+    private void sendConnectedResponse(SocketChannel out) throws IOException {
+        int portInt =  (int)this.headerValues.get("PORT");
+        byte port = (byte) portInt;
 
-        out.write(response);
-        out.flush();
+        byte[] response = new byte[]{0, 90, 0, port, head[4], head[5], head[6], head[7]};
+
+        out.write(ByteBuffer.wrap(response));
         return;
     }
+    private void sendRejectedResponse(SocketChannel out) throws IOException {
+       int portInt =  (int)this.headerValues.get("PORT");
+       byte port = (byte) portInt;
 
+        byte[] response = new byte[]{0, 91, 0, port , head[4], head[5], head[6], head[7]};
+
+        out.write(ByteBuffer.wrap(response));
+        return;
+    }
     private void checkIfAuth(String request) throws IOException {
         BufferedReader reader = new BufferedReader(new StringReader(request));
+        String line; String host = ""; String path = "";
 
-        String line;
-        String host = "";
-        String path = "";
         while ((line = reader.readLine()) != null) {
-
-            System.out.println(line);
             if(line.contains("HTTP")){
                 String[] parts = line.split(" ");
                 path = parts[1];
@@ -67,9 +78,7 @@ public class SockHandler extends Thread {
                 host = parts[1];
             }
             if(line.startsWith("Authorization")){
-                System.out.println(line);
                 String[] parts = line.split(": ");
-                System.out.println(parts);
                 if(parts[1].startsWith("Basic")){
                     String[] basic = parts[1].split(" ");
                     byte[] decodedBytes = Base64.getDecoder().decode(basic[1]);
@@ -82,58 +91,110 @@ public class SockHandler extends Thread {
     }
     private void exit() throws IOException {
         // TODO maybe consider doing a SOCKS closing
-        clientSocket.close();
+        if(clientSocket != null){
+            clientSocket.close();
+        }
+        if(serverSocket != null){
+            serverSocket.close();
+        }
         return;
     }
-    private void mixAndMatch(InputStream inClient, OutputStream outClient, InputStream inServer, OutputStream outServer) throws IOException {
-        byte[] bufferRead = new byte[500];
-        inClient.read(bufferRead);
-        String request = new String(bufferRead, StandardCharsets.UTF_8);
-        checkIfAuth(request);
-        outServer.write(bufferRead);
-        outServer.flush();
-        byte[] bufferWrite = new byte[1500];
-        inServer.read(bufferWrite);
-        String response = new String(bufferWrite, StandardCharsets.UTF_8);
-        checkIfAuth(response);
-        outClient.write(bufferWrite);
-        outClient.flush();
-        return;
+    private Boolean mixing(Selector selector, SocketChannel readFrom, SocketChannel writeTo, Boolean checkAuth){
+
+        try {
+
+            int readyChannels = selector.select();
+            Set<SelectionKey> readyKeys = selector.selectedKeys();
+            readyKeys.clear(); // cleans the keys so they can reappear
+
+            if(readyChannels != 2) return true;
+
+            if(!selector.isOpen()) {
+                return false;//graceful exit since selector was not in use anyway
+            }
+            ByteBuffer read = ByteBuffer.allocate(500);
+            read.clear();
+            int bytesRead = readFrom.read(read);
+            if(bytesRead == -1) return false; //finished
+            read.flip();
+            if(checkAuth){
+                String response = new String(read.array(), StandardCharsets.UTF_8);
+                checkIfAuth(response);
+            }
+            writeTo.write(read);
+            read.clear();
+        } catch (ClosedSelectorException | IOException ex) {
+
+            // selector was closed while being used
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    private void mixAndMatch(SocketChannel clientSocket, SocketChannel serverSocket) throws IOException {
+        Selector s1 = Selector.open();
+        Selector s2 = Selector.open();
+        clientSocket.configureBlocking(false);
+        serverSocket.configureBlocking(false);
+
+        clientSocket.register(s1, SelectionKey.OP_READ);
+        clientSocket.register(s2, SelectionKey.OP_WRITE);
+
+        serverSocket.register(s2, SelectionKey.OP_READ);
+        serverSocket.register(s1, SelectionKey.OP_WRITE);
+
+        while (true){
+           Boolean passed =  mixing(s1, clientSocket, serverSocket, true);
+           Boolean passed2 = mixing(s2, serverSocket, clientSocket, false);
+           if(!passed || !passed2){
+               return;
+           }
+        }
     }
 
     public void run() {
-        System.out.println("On threaaad");
+         try {
 
-        try (
-                PrintWriter outClient = new PrintWriter(clientSocket.getOutputStream(), true);
-                OutputStream outOut = clientSocket.getOutputStream();
-                InputStream inClient = clientSocket.getInputStream();
-        ) {
             String inputLine, outputLine;
-            Map<String,Object> headers = headerParsing(inClient);
-            if((int) headers.get("VERSION") != 4)
+            this.headerValues = headerParsing(clientSocket);
+            if((int) headerValues.get("VERSION") != 4)
             {
-                System.err.println("Version is "+ headers.get("VERSION") + " which is not compatible");
+                System.err.println("Connection error: while parsing request: Unsupported SOCKS protocol version (got "+  headerValues.get("VERSION") +")");
+                sendRejectedResponse(clientSocket);
                 this.exit();
                 return;
             }
-            System.out.println("Command: " + headers.get("COMMAND"));
-            String ipAddress = ((InetAddress) headers.get("IP")).getHostAddress();
-            serverSocket = new Socket(ipAddress, (int)headers.get("PORT") );
-            serverSocket.setSoTimeout(120 );
-            int i = 0;
-            sendResponse(outOut);
-            System.out.println("After sending ack, will start to get request...");
+//            System.out.println("Command: " + headerValues.get("COMMAND"));
+            String ipAddress = ((InetAddress) headerValues.get("IP")).getHostAddress();
+            this.serverSocket = SocketChannel.open();
+            serverSocket.socket().setSoTimeout(120);
+            serverSocket.connect(new InetSocketAddress(ipAddress, (int)headerValues.get("PORT")));
+             SocketAddress clientAddress = clientSocket.socket().getLocalSocketAddress();
+             SocketAddress serverAddress = serverSocket.socket().getRemoteSocketAddress();
+             System.out.println("Succesfull connection from " + clientAddress.toString().replace("/", "") + " to " + serverAddress.toString().replace("/", "") );
+            sendConnectedResponse(clientSocket);
 
-            while (i<100){
-                mixAndMatch(inClient, outOut, serverSocket.getInputStream(), serverSocket.getOutputStream());
-                i++;
-            }
-            outClient.println("menea para mi");
+            mixAndMatch(clientSocket,serverSocket);
+
             clientSocket.close();
-
-        } catch (Exception e) {
+            serverSocket.close();
+            System.out.println("Closing connection from " + clientAddress.toString().replace("/", "") + " to " + serverAddress.toString().replace("/", "") );
+        }
+        catch (ConnectException e){
+            System.err.println("Connection error: while connecting to server:" + e.getMessage());
+            try {
+                sendRejectedResponse(clientSocket);
+                this.exit();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return;
+        }
+        catch (Exception e) {
             e.printStackTrace();
+        }
+        finally {
+
         }
     }
 }
--- src/Sockspy.java
+++ src/Sockspy.java
@@ -1,5 +1,9 @@
+import com.sun.security.ntlm.Server;
+
 import java.net.*;
 import java.io.*;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -14,14 +18,16 @@ public class Sockspy {
         int portNumber = Integer.parseInt(args[0]);
         boolean listening = true;
         // TODO limit to 20 connections.
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+        ExecutorService executor = Executors.newFixedThreadPool(20);
 
-        try (ServerSocket welcomeSocket = new ServerSocket(portNumber)) {
+        ServerSocketChannel welcomeSocket = ServerSocketChannel.open();
+        welcomeSocket.socket().bind(new InetSocketAddress(portNumber));
+        try{
             while (listening) {
-                Socket clientSocket = welcomeSocket.accept();
+                SocketChannel clientSocket = welcomeSocket.accept();
                 Runnable worker = new SockHandler(clientSocket);
-                worker.run();
-//                executor.execute(worker);
+//                worker.run();
+                executor.execute(worker);
             }
         } catch (IOException e) {
             System.err.println("Could not listen on port " + portNumber);
